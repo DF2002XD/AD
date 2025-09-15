9c73a7a8a04f0128c2836574ee8b8b013d55c565
DF2002XD <superyo123456789@gmail.com>
2025-09-15 20:16:23 +0200

    repaso

--- Repaso/Java/102/src/App.java
+++ Repaso/Java/102/src/App.java
@@ -1,5 +1,49 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+
+    private static Scanner sc = new Scanner(System.in);
+    private static int edadjubilacion = 65;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String nombre = leerString("Introduzca el nombre: ");
+        int edad = leerEntero("Introduzca la edad: ");
+
+        if (edad < 18){
+            System.out.println("Es menor de edad");
+        }
+        else if(edad < edadjubilacion) {
+            System.out.println(nombre + " edad de jubilación = 65 años le quedan "+ Integer.toString(edadjubilacion -edad) + " para la jubilación");
+        }else{
+            System.out.println(nombre + " ya está jubilad@, lleva estos añs jubilad@ " + Integer.toString(edad -edadjubilacion));
+        }
+
     }
+
+    private static String leerString(String mensaje) {
+        while (true) {
+            System.out.print(mensaje);
+            String texto = sc.nextLine().trim();
+            if (!texto.isEmpty()) {
+                return texto;
+            } else {
+                System.out.println("Error: El texto no puede estar vacío.");
+            }
+        }
+    }
+
+    private static int leerEntero(String mensaje) {
+        while (true) {
+            try {
+                System.out.print(mensaje);
+                return sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debe introducir un número entero.");
+                sc.nextLine(); // Limpiar buffer
+            }
+        }
+    }
+
 }
