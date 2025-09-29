0a476a098871b9e3fe2998f8b3a2d0dffc04ea44
DF2002XD <superyo123456789@gmail.com>
2025-09-29 20:45:10 +0200

    MYSQL ej303

--- Tema 1/MySQL/303/src/BorradoEmpleados.java
+++ Tema 1/MySQL/303/src/BorradoEmpleados.java
@@ -1,7 +1,11 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import javax.smartcardio.CardTerminals.State;
 
 public class BorradoEmpleados {
     private static Connection db = Database.getInstance();
@@ -10,8 +14,19 @@ public class BorradoEmpleados {
     public static void main(String[] args) {
         try {
             int num = leerEntero("Introduzca el número de empleados a borrar: ");
-            //
             
+            Statement smt = db.createStatement();
+            smt.execute("SET FOREIGN_KEY_CHECKS=0");
+
+            PreparedStatement psmt = db.prepareStatement("DELETE FROM empleado WHERE nss= ?");
+            psmt.setInt(1, num);
+
+            int filasAfectadas = psmt.executeUpdate();
+            System.out.println("Número de empleados borrados: " + filasAfectadas);
+
+            psmt.close();
+            smt.execute("SET FOREIGN_KEY_CHECKS=1");
+            smt.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
