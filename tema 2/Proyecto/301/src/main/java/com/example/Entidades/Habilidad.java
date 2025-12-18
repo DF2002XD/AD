9ab4e6a0b2279b6dde79bc5c4d0fd2978f1d6f6e
DF2002XD <superyo123456789@gmail.com>
2025-12-18 20:22:36 +0100

    Update Hibernate configuration and entity mappings for Evento and Participa

--- tema 2/Proyecto/301/src/main/java/com/example/Entidades/Habilidad.java
+++ tema 2/Proyecto/301/src/main/java/com/example/Entidades/Habilidad.java
@@ -2,19 +2,11 @@ package com.example.Entidades;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.hibernate.annotations.ManyToAny;
-
-import com.google.protobuf.Generated;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
